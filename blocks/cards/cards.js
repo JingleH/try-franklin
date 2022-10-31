f2437157d63ecda1658c32356b7f90391b1f1568
Jingle Huang <jinglhua@adobe.com>
2022-10-31 14:29:16 -0700

    test branch

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -14,5 +14,6 @@ export default function decorate(block) {
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
+  console.log('added a log to the card');
   block.append(ul);
 }
